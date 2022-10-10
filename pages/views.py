fb2e8da8964508d4a6cf1f241df03f664bdd3e03
cake-dev <developer.montana@gmail.com>
2022-10-10 13:29:11 -0600

    fix broken merge

--- pages/views.py
+++ pages/views.py
@@ -8,7 +8,6 @@ import pandas as pd
 import numpy as np
 from oauth2client.service_account import ServiceAccountCredentials
 
-from . import plotly_app as papp
 
 """ def homePageView(request):
 
@@ -36,8 +35,8 @@ def dataHelp():
     return HttpResponse(context)
 
 
-class HomePageView(TemplateView):
-    template_name = "home.html"
+# class HomePageView(TemplateView):
+#    template_name = "home.html"
 
 
 class indexView(TemplateView):
@@ -54,3 +53,41 @@ def dataView(request):
 
 class DashboardView(TemplateView):
     template_name = "dash_test.html"
+
+
+class HomePageView(TemplateView):
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+
+        # defining the scope of the application
+        scope = [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/drive",
+        ]
+
+        # credentials to the account
+        cred = ServiceAccountCredentials.from_json_keyfile_name("auth.json", scope)
+
+        # authorize the clientsheet
+        client = gspread.authorize(cred)
+
+        # open spreadsheet and specific sheet
+        wks = client.open("Test Data umSoccer Test").sheet1
+
+        # wks.update('A1', "test_ian_fin")
+        df = pd.DataFrame(wks.get_all_records())
+        df.head()
+
+        # Call the base implementation first to get a context
+
+        ctx = super().get_context_data(**kwargs)
+
+        # Add your own entry
+
+        ctx["trish"] = "Hello"
+        ctx["ian"] = "Lets go!"
+        ctx["data"] = df["Event Date"][0]
+
+        return ctx
