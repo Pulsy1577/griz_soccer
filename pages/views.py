2c672f2cff5fddde6073851625bc5f47b125d0a2
Ian Davison <idavison157@gmail.com>
2022-11-14 13:04:08 -0700

    time_for_merge!

--- pages/views.py
+++ pages/views.py
@@ -43,7 +43,7 @@ class coachView(TemplateView):
 
 class HomePageView(TemplateView):
 
-    template_name = "registration/login.html"
+    template_name = "home.html"
 
     def get_context_data(self, **kwargs):
 
